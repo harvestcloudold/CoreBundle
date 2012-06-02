55243028410bee34764abcb65fe1ba1f58ad2905
Tom Haskins-Vaughan <tom@harvestcloud.com>
2012-06-02 15:36:50 -0400

    Importing HarvestCloudCoreBundle.php

---
+++ HarvestCloudCoreBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HarvestCloudCoreBundle extends Bundle
+{
+}
