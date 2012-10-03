4f5e1e511987dfc9e013351b2f0b51b67bbdefe8
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2012-10-02 20:52:27 -0400

    Created simple utility class DayOfWeek

---
+++ Util/DayOfWeek.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the Harvest Cloud package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HarvestCloud\CoreBundle\Util;
+
+/**
+ * A utility class to represent Days of the Week
+ *
+ * @author  Tom Haskins-Vaughan <tom@harvestcloud.com>
+ * @since   2012-10-02
+ */
+class DayOfWeek
+{
+    /**
+     * Day of Week numbers
+     *
+     * @var integer
+     */
+    const
+        MON = 1,
+        TUE = 2,
+        WED = 3,
+        THU = 4,
+        FRI = 5,
+        SAT = 6,
+        SUN = 7
+    ;
+
+    /**
+     * Day of week number (e.g. 1 = Mon, 7 = Sun)
+     *
+     * @var integer
+     */
+    protected $day_of_week_number;
+
+    /**
+     * __constuct()
+     *
+     * @author Tom Haskins-Vaughan <tomhv@janeiredale.com>
+     * @since  2012-10-02
+     *
+     * @param  integer  $day_of_week_number
+     */
+    public function __construct($day_of_week_number)
+    {
+        if (!in_array((int) $day_of_week_number, range(1,7)))
+        {
+            throw new Exception('Incorrect parameter');
+        }
+
+        $this->day_of_week_number = $day_of_week_number;
+    }
+
+    /**
+     * getShortName()
+     *
+     * @author Tom Haskins-Vaughan <tomhv@janeiredale.com>
+     * @since  2012-10-02
+     *
+     * @return string
+     */
+    public function getShortName()
+    {
+        switch ($this->day_of_week_number)
+        {
+            case self::MON: return 'Mon';
+            case self::TUE: return 'Tue';
+            case self::WED: return 'Wed';
+            case self::THU: return 'Thu';
+            case self::FRI: return 'Fri';
+            case self::SAT: return 'Sat';
+            case self::SUN: return 'Sun';
+        }
+    }
+
+    /**
+     * getLongName()
+     *
+     * @author Tom Haskins-Vaughan <tomhv@janeiredale.com>
+     * @since  2012-10-02
+     *
+     * @return string
+     */
+    public function getLongName()
+    {
+        switch ($this->day_of_week_number)
+        {
+            case self::MON: return 'Monday';
+            case self::TUE: return 'Tuesday';
+            case self::WED: return 'Wednesday';
+            case self::THU: return 'Thursday';
+            case self::FRI: return 'Friday';
+            case self::SAT: return 'Saturday';
+            case self::SUN: return 'Sunday';
+        }
+    }
+}
