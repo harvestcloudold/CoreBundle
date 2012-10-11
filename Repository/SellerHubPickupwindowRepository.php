5562cc696ebf4d2c2f5f301c7e3d204573c05a2c
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2012-10-10 22:11:16 -0400

    Added SellerHubPickupwindowRepository::findUpcomingForSeller()

--- Repository/SellerHubPickupwindowRepository.php
+++ Repository/SellerHubPickupwindowRepository.php
@@ -3,6 +3,7 @@
 namespace HarvestCloud\CoreBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use HarvestCloud\CoreBundle\Entity\Profile;
 
 /**
  * SellerHubPickupwindowRepository
@@ -33,5 +34,32 @@ class SellerHubPickupwindowRepository extends EntityRepository
         $q = $qb->getQuery();
 
         return $q->getOneOrNullResult();
+    }
+
+    /**
+     * Find upcoming for a given Seller
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2012-10-10
+     *
+     * @param  Profile $seller
+     */
+    public function findUpcomingForSeller(Profile $seller)
+    {
+        $em = $this->getEntityManager();
+
+        $q  = $em->createQuery('
+                SELECT w
+                FROM HarvestCloudCoreBundle:SellerHubPickupWindow w
+                LEFT JOIN w.sellerHubRef shr
+                LEFT JOIN shr.hub h
+                WHERE shr.seller = :seller
+                AND w.end_time >= :now
+            ')
+            ->setParameter('seller', $seller)
+            ->setParameter('now', date('Y-m-d H:i:s'))
+        ;
+
+        return $q->getResult();
     }
 }
