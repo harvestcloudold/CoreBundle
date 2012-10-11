4e4eaec08723126a7b32d883e2ae83c86edd7576
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2012-10-10 21:29:56 -0400

    Added WindowMakerRepository::findForSeller()

--- Repository/WindowMakerRepository.php
+++ Repository/WindowMakerRepository.php
@@ -3,7 +3,7 @@
 namespace HarvestCloud\CoreBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use HarvestCloud\CoreBundle\Entity\Order;
+use HarvestCloud\CoreBundle\Entity\Profile;
 
 /**
  * WindowMakerRepository
@@ -13,4 +13,28 @@ use HarvestCloud\CoreBundle\Entity\Order;
  */
 class WindowMakerRepository extends EntityRepository
 {
+    /**
+     * Find for a given Seller
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2012-10-10
+     *
+     * @param  Profile $seller
+     */
+    public function findForSeller(Profile $seller)
+    {
+        $em = $this->getEntityManager();
+
+        $q  = $em->createQuery('
+                SELECT wm
+                FROM HarvestCloudCoreBundle:WindowMaker wm
+                LEFT JOIN wm.sellerHubRef shr
+                LEFT JOIN shr.hub h
+                WHERE shr.seller = :seller
+            ')
+            ->setParameter('seller', $seller)
+        ;
+
+        return $q->getResult();
+    }
 }
