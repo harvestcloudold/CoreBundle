c35b2b418a1fac5673a36ac79da16438fe6b49d5
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2012-10-16 20:01:41 -0400

    Passing Seller to WindowMakerType in order to get relevant Hubs

--- Form/WindowMakerType.php
+++ Form/WindowMakerType.php
@@ -11,6 +11,8 @@ namespace HarvestCloud\CoreBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use HarvestCloud\CoreBundle\Entity\Profile;
+use HarvestCloud\CoreBundle\Repository\SellerHubRefRepository;
 
 /**
  * WindowMakerType
@@ -21,6 +23,26 @@ use Symfony\Component\Form\FormBuilder;
 class WindowMakerType extends AbstractType
 {
     /**
+     * seller
+     *
+     * @var Profile
+     */
+    protected $seller;
+
+    /**
+     * __construct()
+     *
+     * @author Tom Haskins-Vaugha <tom@harvestcloud.com>
+     * @since  2012-10-15
+     *
+     * @param  Profile  $seller
+     */
+    public function __construct(Profile $seller)
+    {
+        $this->seller = $seller;
+    }
+
+    /**
      * buildForm
      *
      * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
@@ -28,11 +50,19 @@ class WindowMakerType extends AbstractType
      */
     public function buildForm(FormBuilder $builder, array $options)
     {
+        $seller = $this->seller;
+
         $builder
             ->add('sellerHubRef', 'entity', array(
                 'class'    => 'HarvestCloudCoreBundle:SellerHubRef',
                 'property' => 'hubName',
                 'label'    => 'Hub',
+                'query_builder' => function(SellerHubRefRepository $er) use ($seller) {
+                    return $er->createQueryBuilder('shr')
+                        ->where('shr.seller = :seller')
+                        ->setParameter('seller', $seller)
+                    ;
+                },
             ))
             ->add('day_of_week_numbers', 'choice', array(
                 'choices'   => array(
